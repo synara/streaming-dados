c3ac5f637695e78e48d09e1b2e1056672b1f1d3b
Synara <synara-s@hotmail.com>
2021-04-12 18:44:53 -0300

    10a questao incompleta

--- 10q/sparkStreamingKafka.py
+++ 10q/sparkStreamingKafka.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from time import sleep
+import sys
+
 
 # bin/zookeeper-server-start.sh config/zookeeper.properties
 # bin/kafka-server-start.sh config/server.properties
@@ -10,11 +13,12 @@ from pyspark.sql.functions import *
 #bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic twitter
 #bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic facebook
 #bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic instagram
+#bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic other
 
 #bin/kafka-console-producer.sh --broker-list localhost:9092 --topic twitter
 #bin/kafka-console-producer.sh --broker-list localhost:9092 --topic facebook
 #bin/kafka-console-producer.sh --broker-list localhost:9092 --topic instagram
-
+#bin/kafka-console-producer.sh --broker-list localhost:9092 --topic other
 
 if __name__ == "__main__":
 
@@ -26,12 +30,34 @@ if __name__ == "__main__":
     kafka_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("subscribe", "twitter") \
-        .option("subscribe", "facebook") \
-        .option("subscribe", "instagram") \
-        .load()
+        .option("subscribe", "facebook, twitter, instagram, other") \
+        .load()\
+        .selectExpr("CAST(topic AS STRING)", "CAST(value AS STRING)")
 
-    words_df = kafka_df.select(expr("explode(split(value,' ')) as word"))
+    query = kafka_df\
+                        .where("topic <> 'other'")\
+                        .writeStream\
+                        .queryName("networks")\
+                        .format("memory").start()
+
+    words_df = kafka_df.select(expr("explode(split(value,' ')) as word")).where("topic = 'other'")
+    counts_df = words_df.where("word not in ('streaming', 'uni7', 'data', 'science')")
+
+    word_count_query = counts_df.writeStream \
+        .queryName("naoentendo")\
+        .format("memory") \
+        .start()
+            
+    for x in range(50):
+        # spark.sql("SELECT (CASE WHEN TOPIC = 'twitter' THEN '<T>'" \
+        #                        "WHEN TOPIC = 'facebook' THEN '<F>'" \
+        #                        "WHEN TOPIC = 'instagram' THEN '<I>'END) AS ORIGEM, "
+        #                        "value as MENSAGEM " 
+        #                         "FROM networks"
+        # ).show()
+        spark.sql("SELECT * from naoentendo")
+        sleep(5)
 
     
+    query.awaitTermination()
     word_count_query.awaitTermination()
